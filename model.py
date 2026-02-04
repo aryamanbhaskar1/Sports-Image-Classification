449ea139a9000fc6d646cf9c8b21a932cad6945c
aryamanbhaskar1 <asb10078@nyu.edu>
2026-02-04 13:41:04 -0500

    Updated SportsClassifier Head to model.py

--- model.py
+++ model.py
@@ -14,3 +14,20 @@ class ResNetBackbone(nn.Module):
 
     def forward(self, x):
         return self.backbone(x)  # [batch_size, 2048]
+
+# Custom Classifier Head
+class SportsClassifier(nn.Module):
+    def __init__(self, num_classes=100):
+        super().__init__()
+        self.backbone = ResNetBackbone()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        features = self.backbone(x)
+        return self.classifier(features)
